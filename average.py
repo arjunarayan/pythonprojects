00736b76cd06dd492b0744a34f707d4e3373807a
Arjun Raj Narayan <arjunarayan11@gmail.com>
2026-01-26 18:09:46 -0500

    commit

--- average.py
+++ average.py
@@ -2,12 +2,8 @@ def findAverage(numList):
     result = 0
     for num in numList:
 
-    if len(numList) <= 0:
-        return 0
-    else:
-        for i in numList:
-            result += i
-        return result / len(numList)
+        result += num
+    return result/len(numList)
 
 
 print(f"The average of the list you gave is {findAverage(numbs)}")
