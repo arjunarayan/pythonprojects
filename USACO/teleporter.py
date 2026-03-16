3e581ec6ae3123e49dea76ab52c984f99a2ab310
Arjun Raj Narayan <arjunarayan11@gmail.com>
2026-03-15 20:38:12 -0400

    commit

--- USACO/teleporter.py
+++ USACO/teleporter.py
@@ -45,6 +45,7 @@ def teleport():
     distance_one = abs(a-y) + abs(b-x)
     distance_two = abs(a-x) + abs(b-y)
 
+    ans = min(distance_one, distance_two, distance_without_teleporter)
 
 
 print(teleport(3, 10, 8, 2))
