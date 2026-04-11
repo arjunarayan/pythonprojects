2fb79b98c14c072daac33b055a47e781e3a44d14
Arjun Raj Narayan <arjunarayan11@gmail.com>
2026-04-11 12:45:34 -0400

    Delete chatBotCoderSchool directory

--- chatBotCoderSchool/warmup.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.stdout = open("warmup.out", "w")
-for num in range(1, 1001):
-    if num % 10 == 6 or str(num)[len(str(num))-2] + str(num)[len(str(num))-1] == "12":
-        continue
-    else:
-        print(num)
